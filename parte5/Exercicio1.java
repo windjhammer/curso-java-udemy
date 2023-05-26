303fddfaed840dfde4571c64c2b2a4a5ff6b768f
Breno Fernandes <fernandesopa@gmail.com>
2023-05-26 10:18:07 -0300

    exercício 1

--- parte5/Exercicio1.java
+++ parte5/Exercicio1.java
@@ -2,7 +2,16 @@ import java.util.Scanner;
 
 public class Exercicio1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         
+        int entrada;
+        Scanner sc = new Scanner(System.in);
+        entrada = sc.nextInt();
+        if (entrada < 0){
+            System.out.println("NEGATIVO");
+        }
+        else{
+            System.out.println("NÃO NEGATIVO");
+        }
+        sc.close();
     }
 }
