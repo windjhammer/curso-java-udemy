b46b451e2c4a900b761307009376d51fd2ec2ea6
Breno Fernandes <fernandesopa@gmail.com>
2023-05-26 10:05:29 -0300

    readme e exemplo1

---
+++ parte5/Exemplo.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exemplo{
+    public static void main(String[] args) {
+
+        double media;
+
+        Scanner sc = new Scanner(System.in);
+        double nota1 = sc.nextDouble();
+        double nota2 = sc.nextDouble();
+    
+        media = (nota1+nota2);
+
+        if (media < 60) {
+            System.out.printf("NOTA FINAL = %.1f%n", media);
+            System.out.println("REPROVADO");
+        }
+        else{
+            System.out.printf("NOTA FINAL = %.1f%n", media);
+        }
+    }
+}
--- parte5/Readme.md
+++ parte5/Readme.md
@@ -1,3 +1,9 @@
 # Parte 5 do curso
 
 Estruturas condicionais.
+
+## Exemplo
+
+Fazer um programa para ler as duas notas que um aluno obteve no primeiro e segundo semestres de uma disciplina anual. Em seguida, mostrar a nota final que o aluno obteve no ano juntamente com um texto explicativa Caso a nota final do aluno seja inferior a 60.00, mostrar a mensagem "REPROVADO", conforme exemplos. Todos os valores devem ter uma casa decimal.
+
+![img](/exemplo.png)
