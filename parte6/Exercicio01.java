2a9d611794e91387d3e3f894c879d284a06644e1
Breno Fernandes <fernandesopa@gmail.com>
2023-05-31 11:11:49 -0300

    exercicio 01 pronto

--- parte6/Exercicio01.java
+++ parte6/Exercicio01.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Exercicio01 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        int senha = sc.nextInt();
+
+        while(senha != 2002){
+            System.out.println("Senha Inválida");
+            senha  = sc.nextInt();
+        }
+        System.out.println("Acesso Permitido");
+        sc.close();
     }
 }
