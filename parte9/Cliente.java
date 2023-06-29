51fa8986d7e60cc3ed5b70c40b8e9cc5f7faa917
Breno Fernandes <fernandesopa@gmail.com>
2023-06-29 15:03:38 -0300

    parte9 initial

---
+++ parte9/Cliente.java
@@ -0,0 +1,39 @@
+public class Cliente {
+  private int numero;
+  private String titular;
+  private double saldo;
+  
+  public Cliente(int numero, String titular) {
+    this.numero = numero;
+    this.titular = titular;
+  }
+
+  public Cliente(int numero, String titular, double depositoInicial) {
+    this.numero = numero;
+    this.titular = titular;
+    deposito(depositoInicial);
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public String getTitular() {
+    return titular;
+  }
+
+  public void setTitular(String titular) {
+    this.titular = titular;
+  }
+
+  public double getSaldo() {
+    return saldo;
+  }
+
+  public void deposito(double quantidade){
+    saldo += quantidade;
+  }
+  public void saque(double quantidade){
+    saldo -= quantidade + 5.0;
+  }
+}
--- parte9/Program.java
+++ parte9/Program.java
@@ -1,1 +1,11 @@
+package parte9;
 
+import java.util.Scanner;
+
+public class Program{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        sc.close();
+    }
+}
