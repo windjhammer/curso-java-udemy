f02f0d7a010f1afc4c69c015e0b892f01b547191
Breno Fernandes <fernandesopa@gmail.com>
2023-07-24 21:35:54 -0300

    maiorPosicao

---
+++ parte10/maiorPosicao.java
@@ -0,0 +1,32 @@
+package parte10;
+
+import java.util.Scanner;
+
+/**
+ * maiorPosicao
+ */
+public class maiorPosicao {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Quantos números você vai digitar? ");
+        int n = sc.nextInt();
+        int maiorPosicao = 0;
+        double maiorValor = 0;
+        double[] vetor = new double[n];
+        for (int i = 0; i < vetor.length; i++){
+            System.out.println("Digite um número: ");
+            vetor[i] = sc.nextDouble();
+        }
+        for (int i = 0; i<vetor.length; i++){
+            if (vetor[i] > maiorValor){
+                maiorValor = vetor[i];
+                maiorPosicao = i;
+                
+            }
+        }
+        System.out.printf("Maior valor = %.1f\n", maiorValor);
+        System.out.printf("Posição do maior valor = %d\n", maiorPosicao);
+            sc.close();
+    }
+}
