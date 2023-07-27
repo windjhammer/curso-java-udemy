1ad0109c47e2c391fb7996fe15cc7cdbae8c0e69
Breno Fernandes <fernandesopa@gmail.com>
2023-07-26 22:27:33 -0300

    aprovados

---
+++ parte10/Aprovados.java
@@ -0,0 +1,34 @@
+package parte10;
+
+import java.util.Scanner;
+
+/**
+ * Aprovados
+ */
+public class Aprovados {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 0;
+        String aprovado = "";
+        System.out.println("Quantos alunos serão digitados?");
+        n = sc.nextInt();
+        double[] n1 = new double[n];
+        String[] nomes = new String[n];
+        double[] n2 = new double[n];
+        for (int i = 0; i < n1.length; i++) {
+            System.out.printf("Digite nome, primeira e segunda nota do %do aluno\n", i + 1);
+            nomes[i] = sc.next();
+            n1[i] = sc.nextDouble();
+            n2[i] = sc.nextDouble();
+        }
+        System.out.println("Alunos aprovados: ");
+        for (int i = 0; i < n1.length; i++) {
+            if((n1[i]+n2[i])/2 >= 6){
+                aprovado = nomes[i];
+                System.out.println(aprovado);
+            }
+        }
+        sc.close();
+    }
+}
